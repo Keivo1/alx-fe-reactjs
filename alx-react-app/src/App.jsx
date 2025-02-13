455c77163f189a4230c4c6028eb6c70103ab921b
Victor Ikpe <victorikpe5@gmail.com>
2025-02-14 00:32:44 +0100

    react

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './WelcomeMessage.jsx'
+import WelcomeMessage from './components/WelcomeMessage.jsx'
 import Header from './components/Header.jsx'
 import MainContent from './components/MainContent.jsx'
 import Footer from './components/Footer.jsx'
+import UserProfile from './components/UserProfile.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
   return (
     <>
       <div>
+        <UserProfile name="Ikpe Victor" age={26} bio="I'm a novice in React" />
         <Header />
         <MainContent />
         <Footer />
