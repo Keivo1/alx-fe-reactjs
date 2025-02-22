40ab7ce982917b87cfba135010a013f869364b0f
Victor Ikpe <victorikpe5@gmail.com>
2025-02-22 21:40:05 +0100

    react

---
+++ alx-react-app-props/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer
+      style={{ backgroundColor: "navy", color: "white", textAlign: "center", padding:"10px", margin:"5px"}}
+    >
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -0,0 +1,9 @@
+import UserInfo from "./UserInfo";
+import UserContext from "./UserContext";
+
+function ProfilePage() {
+  const userData = useContext(UserContext);
+  return <UserInfo userData={userData} />;
+}
+
+export default ProfilePage;
