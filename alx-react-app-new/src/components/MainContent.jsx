9cf27d3dfb66727e03376a70f5c157fc1f194fa4
Victor Ikpe <victorikpe5@gmail.com>
2025-02-22 13:40:07 +0100

    react

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function MainContent() {
+  return (
+    <main
+      style={{
+        backgroundColor: "teal",
+        color: "skyblue",
+        textAlign: "center",
+        padding: "10px",
+        margin: "5px",
+      }}
+    >
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
