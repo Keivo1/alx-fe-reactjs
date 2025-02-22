9422d736e856115c1c74fa24da343bc43d46a111
Victor Ikpe <victorikpe5@gmail.com>
2025-02-22 13:06:19 +0100

    react

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +14,11 @@ function App() {
   return (
     <>
       <div>
+        <Header/>
+        <WelcomeMessage />
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+        <MainContent />
+        <Footer/>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
