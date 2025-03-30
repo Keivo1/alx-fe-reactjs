017ede2612ddd69116215db7af366181549e6c46
Victor Ikpe <victorikpe5@gmail.com>
2025-03-30 13:28:14 +0100

    github user search

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,16 +2,23 @@ import axios from 'axios';
 
 const GITHUB_API = 'https://api.github.com';
 
-export const fetchUserData = async (username) => {
+export const fetchAdvancedSearch = async ({ username, location, minRepos }) => {
+  let query = '';
+
+  if (username) query += `${username} in:login `;
+  if (location) query += `location:${location} `;
+  if (minRepos) query += `repos:>=${minRepos} `;
+
   try {
-    const response = await axios.get(`${GITHUB_API}/users/${username}`, {
+    const response = await axios.get(`${GITHUB_API}/search/users`, {
+      params: { q: query.trim() },
       headers: {
         Authorization: `Bearer ${import.meta.env.VITE_APP_GITHUB_API_KEY}`,
       },
     });
 
-    return response.data;
+    return response.data.items; 
   } catch (error) {
-    throw new Error('User not found');
+    throw new Error('Search failed');
   }
 };
