c29b39fcb2fbb2ca1be2f23a9a154d54b7f44c76
Victor Ikpe <victorikpe5@gmail.com>
2025-03-30 13:23:30 +0100

    github user search

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -18,7 +18,7 @@ const Search = () => {
       setUserData(data);
     } catch (err) {
  
-      setError('Looks like we can’t find the user');
+      setError("Looks like we cant find the user");
     } finally {
       setLoading(false);
     }
